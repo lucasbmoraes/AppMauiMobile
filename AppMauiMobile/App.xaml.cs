cd231e924b857e165ffb3e03cd98361c9a4dbf23
Lucas Moraes <lucasmoraes86@hotmail.com>
2024-04-21 21:56:39 -0300

    Adicionar arquivos de projeto.

---
+++ AppMauiMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppMauiMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppMauiMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppMauiMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
